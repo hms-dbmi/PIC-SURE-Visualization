3e4c100234a017351d56fb956ea119e9edc0d7e4
James <Jamestp19@gmail.com>
2026-06-02 00:30:01 -0400

    Remove ObfuscationParser, DistributionProcessingOptions, and obfuscation.* config (ALS-10714)

--- src/main/java/edu/harvard/dbmi/avillach/visualization/model/DistributionProcessingOptions.java
+++
@@ -1,7 +0,0 @@
-package edu.harvard.dbmi.avillach.visualization.model;
-
-public record DistributionProcessingOptions(boolean aggregateCategoricalValues, boolean binContinuousValues) {
-
-    public static final DistributionProcessingOptions AUTHORIZED = new DistributionProcessingOptions(false, false);
-    public static final DistributionProcessingOptions OPEN = new DistributionProcessingOptions(false, false);
-}
